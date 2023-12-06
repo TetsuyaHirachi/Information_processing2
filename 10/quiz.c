5fcb4d07a53e2d4935b6f91e82fc2273198d2683
TetsuyaHirachi <bp23083@shibaura-it.ac.jp>
2023-12-06 12:25:41 +0900

    add：strcmpを使った9.cの改良版の追加、それぞれのtxtファイルの追加 update：コメントの追加、コードの最適化

--- 10/quiz.c
+++ 10/quiz.c
@@ -1,24 +1,24 @@
 /*
 QUIZ:10
-作成年月日：2023/12/5
+作成年月日：2023/12/6
 学籍番号：bp23083
 氏名：平地哲也
 */
 
 #include <stdio.h>
+// 配列に九九を入力
 int func(int x[][9], int n) {
-    int i, j;
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < 9; j++) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < 9; j++) {
             x[i][j] = (i + 1) * (j + 1);
         }
     }
 }
 
+// 九九が入った配列の表示
 int show(int x[][9], int n) {
-    int i, j;
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < 9; j++) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < 9; j++) {
             printf("%5d", x[i][j]);
         }
         printf("\n");
